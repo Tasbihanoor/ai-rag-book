cb3209cc87a1834c640c9979f7ded14f84e5c937
Tasbihanoor <noortasbiha5@gmail.com>
2025-12-06 10:33:27 +0500

    new data

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,8 +11,8 @@ const config: Config = {
     v4: true,
   },
 
-  url: 'https://github.com/Tasbihanoor.github.io',
-  baseUrl: '/',
+  url: 'https://tasbihanoor.github.io',
+  baseUrl: '/ai-rag-book/',
 
   organizationName: 'Tasbihanoor',
   projectName: 'ai-book',
